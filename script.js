9fe73aacf8735bd22534bf720826d704dcdecccc
Amie Jensen <amie33@juno.com>
2021-03-03 14:54:31 -0700

    added 3 new transitions to pages

--- script.js
+++ script.js
@@ -2,19 +2,20 @@
 window.onload = () => {
     
     const transitionElement = document.querySelector('.transition');//reference to transitions
-    const anchors = document.querySelectorAll('a');
+    const anchors = document.querySelectorAll('a');//grab all links on the page
 
     setTimeout(() => {
         transitionElement.classList.remove('is-active');
     }, 500); 
 
+    //loop through the anchors(links) on the page
     for(let i = 0; i < anchors.length; i++){
         const anchor = anchors[i];
         anchor.addEventListener('click', e => {
-            e.preventDefault();
-            let target = e.target.href;
+            e.preventDefault();//wont link you to a different page
+            let target = e.target.href;//takes you to the link you're supposed to go to 
 
-            transitionElement.classList.add('is-active');
+            transitionElement.classList.add('is-active');//adds the overlay on top of page
             setTimeout(()=>{
                 window.location.href = target;
             }, 500)            
