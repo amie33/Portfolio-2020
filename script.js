1d8b44352e37df761ae9d6c53c80cea5424509a8
Amie Jensen <amie33@juno.com>
2021-03-26 13:11:45 -0600

    figured out how to show the preloader once, trying to figure out how long i want it showing

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 //when the window is refreshed 
 window.onload = () => {
-    
+
     const transitionElement = document.querySelector('.transition');//reference to transitions
     const anchors = document.querySelectorAll('a');//grab all links on the page
+    const bubbleGum = document.getElementById('bubblegum');//get the bubblegum div
 
     setTimeout(() => {
         transitionElement.classList.remove('is-active');
-    }, 500); 
+    }, 550); //get the transition div and remove it if present
+
+    if(sessionStorage.bubbleGum = !true){
+        sessionStorage.bubbleGum = true;
+        bubbleGum.style.display = 'block';
+    }else{
+        bubbleGum.style.display = 'none';
+    }
 
     //loop through the anchors(links) on the page
     for(let i = 0; i < anchors.length; i++){
-        const anchor = anchors[i];
+        const anchor = anchors[i]; //set anchor to each iteration of a 
+        //every time a link is clicked nothing will happen 
         anchor.addEventListener('click', e => {
             e.preventDefault();//wont link you to a different page
-            let target = e.target.href;//takes you to the link you're supposed to go to 
+            
+            let target = e.target.href;//takes you to the link you're supposed to go to and stores it in target.  T
 
-            transitionElement.classList.add('is-active');//adds the overlay on top of page
+            transitionElement.classList.add('is-active');//adds the overlay on top of page..the default is still being prevented so below we set a timeout for the same amount of time the animation lasts for.  
             setTimeout(()=>{
                 window.location.href = target;
-            }, 500)            
+            }, 500)   
+                    
         });
     }
 }
